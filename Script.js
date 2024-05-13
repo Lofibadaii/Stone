7795c5123b6ebb089844695faa57dae9f52c49b0
Lofibadaii <lofidark0@gmail.com>
2024-05-13 11:35:55 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,20 @@
+const miningButton = document.getElementById('miningButton');
+const buyButton = document.getElementById('buyButton');
+const walletAddressInput = document.getElementById('walletAddress');
+const inviteButton = document.getElementById('inviteButton');
+
+miningButton.addEventListener('click', startMining);
+buyButton.addEventListener('click', buyHash);
+inviteButton.addEventListener('click', inviteFriends);
+
+function startMining() {
+    // Add code for starting mining process
+}
+
+function buyHash() {
+    // Add code for buying hash with Ton
+}
+
+function inviteFriends() {
+    // Add code for inviting friends
+}
